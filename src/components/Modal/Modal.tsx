73aace1a231e03ff61f8f6f6c587d65f7f82eef6
Marina <marinamaksimovskaa3@gmail.com>
2025-09-21 10:27:55 +0300

    hw-5

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,48 @@
+// src/components/NoteModal/NoteModal.tsx
+import css from "./Modal.module.css";
+import { createPortal } from "react-dom";
+import { useEffect } from "react";
+
+interface NoteModalProps {
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+export default function NoteModal({ onClose, children }: NoteModalProps) {
+  const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    // Блокуємо прокрутку
+    const prevOverflow = document.body.style.overflow;
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      document.body.style.overflow = prevOverflow; // Відновлюємо попереднє значення
+    };
+  }, [onClose]);
+
+  return createPortal(
+    <div
+      className={css.backdrop}
+      role="dialog"
+      aria-modal="true"
+      onClick={handleBackdropClick}
+    >
+      <div className={css.modal}>{children}</div>
+    </div>,
+    document.body
+  );
+}
